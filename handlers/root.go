eba4e524f5f97129f0ca24be35e3f2534d29ecb1
Dan Grøndahl Glavind <dangrondahl@gmail.com>
2021-04-15 21:26:08 +0200

    Versioning (#3)
    * adding versioning files
    
    * add changes to dockerfile and workflow

--- handlers/root.go
+++ handlers/root.go
@@ -1,12 +1,32 @@
 package handlers
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/dangrondahl/hello-go-app/version"
 )
 
 func root(w http.ResponseWriter, r *http.Request) {
+	info := struct {
+		BuildTime string `json:"buildTime"`
+		Commit    string `json:"commit"`
+		Release   string `json:"version"`
+	}{
+		version.BuildTime, version.Commit, version.Version,
+	}
+
+	body, err := json.Marshal(info)
+
+	if err != nil {
+		log.Printf("Could not encode info data: %v", err)
+		http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(body)
+
 	log.Printf("Hello world received a request.")
-	fmt.Fprint(w, "Hello World: Example!")
 }
